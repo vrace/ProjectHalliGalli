54deb66b9a20718014388cca0dfa7f53dee39859
vrace <vrace_studios@hotmail.com>
2015-07-12 18:39:37 +0800

    move unneeded thingy from render

--- src/HalliGalli/Framework/Render/Render.cpp
+++ src/HalliGalli/Framework/Render/Render.cpp
@@ -1,16 +1,6 @@
 #include "Render.h"
 #include <stdio.h>
 
-namespace
-{
-	void SubmitVertex(const RenderVertex &v)
-	{
-		glColor4f(v.color().r(), v.color().g(), v.color().b(), v.color().a());
-		glTexCoord2f(v.uv().u(), v.uv().v());
-		glVertex3f(v.vertex().x(), v.vertex().y(), v.vertex().z());
-	}
-}
-
 Render::Render()
 	: _texture(0)
 {
