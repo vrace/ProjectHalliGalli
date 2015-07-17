bed69c05fddd32168a7328270369a609fc91c4c1
vrace <vrace_studios@hotmail.com>
2015-07-17 23:45:53 +0800

    add xaml types

---
+++ src/HalliGalli/Framework/Xaml/XamlTypes.h
@@ -0,0 +1,81 @@
+#ifndef __XAML_TYPES_H__
+#define __XAML_TYPES_H__
+
+#include <string>
+
+class XamlTransformOrigin
+{
+public:
+	XamlTransformOrigin()
+		: x(0.5f)
+		, y(0.5f)
+	{
+	}
+
+	float x;
+	float y;
+};
+
+class XamlScaleTransform
+{
+public:
+	XamlScaleTransform()
+		: scaleX(1)
+		, scaleY(1)
+	{
+	}
+
+	float scaleX;
+	float scaleY;
+};
+
+class XamlRotateTransform
+{
+public:
+	XamlRotateTransform()
+		: angle(0)
+	{
+	}
+
+	float angle;
+};
+
+class XamlTranslateTransform
+{
+public:
+	XamlTranslateTransform()
+		: x(0)
+		, y(0)
+	{
+	}
+
+	float x;
+	float y;
+};
+
+class XamlTransformGruop
+{
+public:
+	XamlScaleTransform scale;
+	XamlRotateTransform rotate;
+	XamlTranslateTransform translate;
+};
+
+class XamlWindow
+{
+public:
+	int width;
+	int height;
+};
+
+class XamlImage
+{
+public:
+	std::string source;
+	int width;
+	int height;
+	XamlTransformOrigin origin;
+	XamlTransformGroup transform;
+};
+
+#endif
